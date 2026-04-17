48e68f33db7f65fef9dca3f74c5c3be3ac138633
Cathleya Khalbi <reincath.bi@gmail.com>
2026-04-17 22:10:20 +0700

    Refactor Buku class to use instance variables

--- LK 06/Buku.java
+++ LK 06/Buku.java
@@ -1,40 +1,32 @@
+import java.util.Scanner;
+
 public class Buku {
-    private static String kode;
-    private static String judul;
-    private static String jenis;
+    private String kode;
+    private String judul;
+    private String jenis;
 
     public Buku(String kode, String judul, String jenis) {
-        Buku.kode = kode;
-        Buku.judul = judul;
-        Buku.jenis = jenis;
+        this.kode = kode;
+        this.judul = judul;
+        this.jenis = jenis;
     }
 
-    public void setJudul(String judul) {
-        Buku.judul = judul;
-    }
-
-    public void setJenis(String jenis) {
-        Buku.jenis = jenis;
-    }
-
-    public String getKode() {
-        return kode;
-    }
-
-    public String getJudul() {
-        return judul;
-    }
-
-    public String getJenis() {
-        return jenis;
-    }
-
-    public static String toFile() {
+    public String toFile() {
         return kode + "|" + judul + "|" + jenis;
     }
 
     public static void tambahBuku() {
-        FileHelper.write("buku.txt", toFile());
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Kode: ");
+        String kode = input.nextLine();
+        System.out.print("Judul: ");
+        String judul = input.nextLine();
+        System.out.print("Jenis: ");
+        String jenis = input.nextLine();
+
+        Buku b = new Buku(kode, judul, jenis);
+        FileHelper.write("buku.txt", b.toFile());
     }
 
     public static void tampilBuku() {
