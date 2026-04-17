4ba13f89a71d5ec2f1ed22c531ee3ba678914ebb
Cathleya Khalbi <reincath.bi@gmail.com>
2026-04-17 22:02:23 +0700

    Refactor MainApp to initialize files and handle login

--- LK 06/MainApp.java
+++ LK 06/MainApp.java
@@ -1,10 +1,55 @@
-public class MainApp {
+import java.io.File;
+
+public class Main {
     public static void main(String[] args) {
+
+        System.out.println("    SISTEM PERPUSTAKAAN    ");
+
+        try {
+            // buat file
+            File pegawai = new File("pegawai.txt");
+            File siswa = new File("siswa.txt");
+            File buku = new File("buku.txt");
+            File transaksi = new File("transaksi.txt");
+
+            pegawai.createNewFile();
+            siswa.createNewFile();
+            buku.createNewFile();
+            transaksi.createNewFile();
+
+            // isi data awal
+            if (pegawai.length() == 0) {
+                Pegawai p = new Pegawai("123", "admin", new java.util.Date());
+                FileHelper.write("pegawai.txt", p.toFile());
+                System.out.println("Admin dibuat");
+            }
+
+            if (siswa.length() == 0) {
+                Siswa s1 = new Siswa("001", "Budi", "Malang");
+                Siswa s2 = new Siswa("002", "Siti", "Blitar");
+
+                FileHelper.write("siswa.txt", s1.toFile());
+                FileHelper.write("siswa.txt", s2.toFile());
+            }
+
+            if (buku.length() == 0) {
+                Buku b1 = new Buku("B01", "Laskar Pelangi", "Novel");
+                Buku b2 = new Buku("B02", "Fisika Dasar", "Pelajaran");
+
+                FileHelper.write("buku.txt", b1.toFile());
+                FileHelper.write("buku.txt", b2.toFile());
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error inisialisasi file");
+        }
+
+        // login
         if (Admin.login()) {
+            System.out.println("Login berhasil!");
             Admin.menu();
         } else {
-            System.out.println("Gagal login");
+            System.out.println("Login gagal!");
         }
     }
 }
-    
