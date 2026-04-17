fc5e1559e5533f04f09fcfed41ac4aea67c86d45
Cathleya Khalbi <reincath.bi@gmail.com>
2026-04-17 23:59:11 +0700

    Refactor tambahBuku to use MainApp input

--- LK 06/Buku.java
+++ LK 06/Buku.java
@@ -16,8 +16,7 @@ public class Buku {
     }
 
     public static void tambahBuku() {
-        Scanner input = new Scanner(System.in);
-
+        Scanner input = MainApp.input;
         System.out.print("Kode: ");
         String kode = input.nextLine();
         System.out.print("Judul: ");
@@ -27,9 +26,11 @@ public class Buku {
 
         Buku b = new Buku(kode, judul, jenis);
         FileHelper.write("buku.txt", b.toFile());
+        System.out.println("Buku berhasil ditambahkan!");
     }
 
     public static void tampilBuku() {
+        System.out.println("Kode | Judul | Jenis");
         FileHelper.read("buku.txt");
     }
 }
